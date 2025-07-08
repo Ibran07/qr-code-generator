961799f1feaa913ca682b1cd02463c0e9f276881
Ibran <iamibran@gmail.com>
2025-07-08 22:11:42 +0530

    Adding logic to download button

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,13 @@ const qrImage = document.querySelector(".qrImage");
 const inputContainer = document.querySelector(".inputContainer");
 const qrContainer = document.querySelector(".qrContainer");
 
+//Share and Download Button 
+const downloadBtn = document.querySelector("#download");
+const shareBtn = document.querySelector("#share");
+
 try {
     form.addEventListener("submit", generateQR);
+    downloadBtn.addEventListener("click", download);
 } catch (err){
     console.log("Error is ",err);
 }
@@ -24,4 +29,18 @@ async function generateQR(e) {
     });
     inputContainer.style.display = "none";
     qrContainer.style.display = "flex";
+}
+
+function download() {
+    const img = qrImage.querySelector("img");
+
+    if (!img){
+        console.log("QR Code is not generator yet!");
+        return;
+    }
+
+    const link = document.createElement("a");
+    link.href = img.src;
+    link.download = "QR-Code";
+    link.click();
 }
